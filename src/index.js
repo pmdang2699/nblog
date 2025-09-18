49d4d4d796f3e8ba2d1453146bbda5c621c98035
PhamMinhDang <pmdang2699@gmail.com>
2025-09-18 11:58:28 +0700

    update file moi

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.use(morgan('combined'));
 
 app.engine('.hbs', engine({extname: '.hbs'}));
