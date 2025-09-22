002d2ee58cf3be10c4f804e18d22eb01a34d5fb7
PhamMinhDang <pmdang2699@gmail.com>
2025-09-22 13:25:27 +0700

    update file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.use(morgan('combined'));
 
@@ -13,13 +18,9 @@ app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// route init
+route(app);
+    
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
